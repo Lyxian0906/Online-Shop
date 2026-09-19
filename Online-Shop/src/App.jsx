835cc419de33dc9733695369e69af3e5766f555a
Lyx <martadiamond2@gmail.com>
2026-09-19 17:10:54 +0200

    Added shortcuts

--- Online-Shop/src/App.jsx
+++ Online-Shop/src/App.jsx
@@ -4,7 +4,8 @@ import { Routes, Route } from "react-router";
 function App() {
 	return (
 		<Routes>
-      <Route path="/" element={<HomePage />}></Route>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<div>test</div>} />
 		</Routes>
 	);
 }
