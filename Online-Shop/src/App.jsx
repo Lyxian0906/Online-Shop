35a565ac117c1387c97f485252ce1c7ead0585d6
Lyx <martadiamond2@gmail.com>
2026-09-19 17:16:48 +0200

    Added checkoutpage

--- Online-Shop/src/App.jsx
+++ Online-Shop/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from './pages/CheckoutPage'
 import { Routes, Route } from "react-router";
 function App() {
 	return (
 		<Routes>
       <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<div>test</div>} />
+      <Route path="/checkout" element={<CheckoutPage />} />
 		</Routes>
 	);
 }
