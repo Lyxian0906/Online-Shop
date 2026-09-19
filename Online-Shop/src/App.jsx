a46d7804048e54ff339e8eed9fc2350b150c9066
Lyx <martadiamond2@gmail.com>
2026-09-19 17:40:34 +0200

    Added shortcuts

--- Online-Shop/src/App.jsx
+++ Online-Shop/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import { HomePage } from "./pages/HomePage";
-import { CheckoutPage } from './pages/CheckoutPage'
+import { HomePage } from "./pages/home_page/HomePage";
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { Routes, Route } from "react-router";
 function App() {
 	return (
