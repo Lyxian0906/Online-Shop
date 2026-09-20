3f54295ab1ff8fb0778213dfe2b646dc8dc538b3
Lyx <martadiamond2@gmail.com>
2026-09-21 01:00:38 +0200

    Added back products files, and started backend with fetch

--- Online-Shop/src/pages/home_page/HomePage.jsx
+++ Online-Shop/src/pages/home_page/HomePage.jsx
@@ -4,6 +4,11 @@ import "./HomePage.css";
 
 export function HomePage() {
 	//We can't return 2 pages so we wrap it into a segment
+	fetch('http://localhost:3000/api/products')
+	.then((response) => {
+		console.log(response)
+
+	})
 	return (
 		<>
 			<Header />
