a678a3c25005fe934a05aab9f1e28918f2177004
Lyx <martadiamond2@gmail.com>
2026-09-20 17:14:51 +0200

    Routing orders

--- Online-Shop/src/App.jsx
+++ Online-Shop/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { HomePage } from "./pages/home_page/HomePage";
-import { CheckoutPage } from './pages/checkout/CheckoutPage'
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { Routes, Route } from "react-router";
+import { OrdersPage } from "./pages/orders/OrdersPage";
+
 function App() {
 	return (
 		<Routes>
-      <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
+			<Route index element={<HomePage />} />
+			<Route path="/checkout" element={<CheckoutPage />} />
+			<Route path="/orders" element={<OrdersPage />} />
 		</Routes>
 	);
 }
