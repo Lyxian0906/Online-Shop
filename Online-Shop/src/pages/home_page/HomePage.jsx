03dd9acdc139979972c1599f7d1e6ac2973d59a5
Lyx <martadiamond2@gmail.com>
2026-09-21 01:06:00 +0200

    Fetch data from backend

--- Online-Shop/src/pages/home_page/HomePage.jsx
+++ Online-Shop/src/pages/home_page/HomePage.jsx
@@ -4,11 +4,13 @@ import "./HomePage.css";
 
 export function HomePage() {
 	//We can't return 2 pages so we wrap it into a segment
-	fetch('http://localhost:3000/api/products')
-	.then((response) => {
-		console.log(response)
-
-	})
+	fetch("http://localhost:3000/api/products")
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			
+		});
 	return (
 		<>
 			<Header />
@@ -19,12 +21,9 @@ export function HomePage() {
 					{products.map((product) => {
 						return (
 							<>
-								<div key={product.id}className="product-container">
+								<div key={product.id} className="product-container">
 									<div className="product-image-container">
-										<img
-											className="product-image"
-											src={product.image}
-										/>
+										<img className="product-image" src={product.image} />
 									</div>
 
 									<div className="product-name limit-text-to-2-lines">
@@ -36,10 +35,14 @@ export function HomePage() {
 											className="product-rating-stars"
 											src={`"images/ratings/rating-${product.rating.stars * 10}.png"`}
 										/>
-										<div className="product-rating-count link-primary">{product.rating.count}</div>
+										<div className="product-rating-count link-primary">
+											{product.rating.count}
+										</div>
 									</div>
 
-									<div className="product-price">${(product.priceCents / 100).toFixed(2)}</div> 
+									<div className="product-price">
+										${(product.priceCents / 100).toFixed(2)}
+									</div>
 
 									<div className="product-quantity-container">
 										<select>
