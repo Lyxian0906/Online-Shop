4f685e0b20aa2d3d770c8c541eb316194f90e4e9
Lyx <martadiamond2@gmail.com>
2026-09-22 18:59:28 +0200

    Fixed rating

--- Online-Shop/src/pages/home_page/HomePage.jsx
+++ Online-Shop/src/pages/home_page/HomePage.jsx
@@ -35,7 +35,7 @@ export function HomePage() {
 									<div className="product-rating-container">
 										<img
 											className="product-rating-stars"
-											src={`"images/ratings/rating-${product.rating.stars * 10}.png"`}
+											src={`/images/ratings/rating-${product.rating.stars * 10}.png`}
 										/>
 										<div className="product-rating-count link-primary">
 											{product.rating.count}
