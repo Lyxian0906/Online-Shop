a4bded67827ff99792365dc2ba8c2c80ba4ee169
Lyx <martadiamond2@gmail.com>
2026-09-25 00:24:32 +0200

    Testing quantity selected

--- Online-Shop/src/pages/home_page/components/ProductsGrid.jsx
+++ Online-Shop/src/pages/home_page/components/ProductsGrid.jsx
@@ -1,7 +1,9 @@
 import { formatMoney } from "../../../utils/money";
+import { useState } from "react";
 import axios from "axios";
 
 export function ProductsGrid({products, loadCart}) {
+	const [quantity, setQuantity] = useState(1); 
 	return (
 		<div className="products-grid">
 			{products.map((product) => {
@@ -31,7 +33,12 @@ export function ProductsGrid({products, loadCart}) {
 							</div>
 
 							<div className="product-quantity-container">
-								<select>
+								<select value={quantity} onChange={(event) =>{
+									//Convert string into number
+									const quantitySelected = Number(event.target.value)
+									setQuantity(quantitySelected);
+									console.log(quantitySelected)
+								}}>
 									<option value="1">1</option>
 									<option value="2">2</option>
 									<option value="3">3</option>
