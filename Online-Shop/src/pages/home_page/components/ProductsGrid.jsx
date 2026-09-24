165585107c922d7e37329737a166906d3d8f2f78
Lyx <martadiamond2@gmail.com>
2026-09-25 00:12:03 +0200

    Added functionality to add to cart button

--- Online-Shop/src/pages/home_page/components/ProductsGrid.jsx
+++ Online-Shop/src/pages/home_page/components/ProductsGrid.jsx
@@ -1,4 +1,5 @@
 import { formatMoney } from "../../../utils/money";
+import axios from "axios";
 
 export function ProductsGrid({products}) {
 	return (
@@ -51,7 +52,12 @@ export function ProductsGrid({products}) {
 								Added
 							</div>
 
-							<button className="add-to-cart-button button-primary">
+							<button className="add-to-cart-button button-primary" onClick={() =>{
+								axios.post('/api/cart-items', {
+									productId: product.id,
+									quantity: 1
+								});
+							}}>
 								Add to Cart
 							</button>
 						</div>
