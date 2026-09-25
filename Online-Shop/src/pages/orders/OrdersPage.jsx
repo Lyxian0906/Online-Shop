a1bd4300936fde1f79833be3cfd8878d03cb6691
Lyxian0906 <martadiamond2@gmail.com>
2026-09-25 13:15:50 +0200

    Made minor changes(need to be fixed)

--- Online-Shop/src/pages/orders/OrdersPage.jsx
+++ Online-Shop/src/pages/orders/OrdersPage.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect, Fragment } from "react";
 import { Link } from "react-router";
 import { formatMoney } from "../../utils/money";
 import { OrderDetailGrid } from "./components/OrdersDetailGrid";
+import { OrdersGrid } from "./components/OrdersGrid";
 
 export function OrdersPage({ cart }) {
 	const [orders, setOrders] = useState([]);
@@ -21,34 +22,8 @@ export function OrdersPage({ cart }) {
 
 			<div className="orders-page">
 				<div className="page-title">Your Orders</div>
-
-				<div className="orders-grid">
-					{orders.map((order) => {
-						return (
-							<div key={order.id} className="order-container">
-								<div className="order-header">
-									<div className="order-header-left-section">
-										<div className="order-date">
-											<div className="order-header-label">Order Placed:</div>
-											<div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-										</div>
-										<div className="order-total">
-											<div className="order-header-label">Total:</div>
-											<div>{formatMoney(order.totalCostCents)}</div>
-										</div>
-									</div>
-
-									<div className="order-header-right-section">
-										<div className="order-header-label">Order ID:</div>
-										<div>{order.id}</div>
-									</div>
-								</div>
-								<OrderDetailGrid order={order} />
-								
-							</div>
-						);
-					})}
-				</div>
+				<OrdersGrid order={order}/>
+				
 			</div>
 		</>
 	);
